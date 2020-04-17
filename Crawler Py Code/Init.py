8e7a584c2aae8a8dd6190b56ea2624ca575121aa
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-04-17 15:25:06 +0900

    초기화 코드 생성

---
+++ Crawler Py Code/Init.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-
+
+import threading
+from initCode import init_crawl
+from initCode import init_Foreigner_invest_crawl
+from initCode import init_NPS_invest_crawl
+
+init_crawl.init_crawl()
+
+print('KOSPI Crawling done.')
+print('Foreigner & NPS Crawling Start')
+
+foreign = threading.Thread(target=init_Foreigner_invest_crawl.init_Foreigner_invest_crawl)
+nps = threading.Thread(target=init_NPS_invest_crawl.init_NPS_invest_crawl)
+
+foreign.start()
+nps.start()
+
+print('Crawling Finished')
