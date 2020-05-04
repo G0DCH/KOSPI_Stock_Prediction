96f5f97b722bc969e5fff0c2f521046c6c2c354e
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-05-04 23:43:48 +0900

    예제 코드로 수정, 다음날 예측 추가

--- Crawler Py Code/LSTM/Run.py
+++ Crawler Py Code/LSTM/Run.py
@@ -21,23 +21,23 @@ start = time.time()
 emptyFrame = pd.DataFrame(columns = ['현재가', '외인순매수거래량', 
                                     '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금'])
 
-"""
+pivotDatas = []
+
 # 입력 받은 데이터를 정규화함
 def Normalize(dataList):
     normalizedDatas = []
 
     start = time.time()
     print('Normalize Start')
+    global pivotDatas
 
     for window in dataList:
         normalizedWindow = window.copy()
         pivot = window.copy()
+        pivotDatas.append(pivot.iloc[0, 0])
         for i in range(len(pivot)):
             pivot.iloc[i] = pivot.iloc[0]
         normalizedWindow.loc[:] = window.loc[:] / pivot[:] - 1
-        #normalizedWindow.loc[:, ['현재가', '외인순매수거래량', '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']] \
-        #            = window.loc[:, ['현재가', '외인순매수거래량', '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']] / \
-        #            pivot.loc[:, ['현재가', '외인순매수거래량', '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']] - 1
         normalizedDatas.append(normalizedWindow.values.tolist())
 
     result = np.array(normalizedDatas)
@@ -46,11 +46,11 @@ def Normalize(dataList):
     print('Normalized time : ' + str(timedelta(seconds = time.time() - start)))
 
     return result
-"""
+
 
 # 학습 데이터 셋 만듬
 def LoadData(window_Size):
-
+    
     start = time.time()
     print('Load Data Start')
 
@@ -62,12 +62,13 @@ def LoadData(window_Size):
     ReconstructPath = os.path.join(path, ReconstructDirName)
     dataFileNameList = os.listdir(ReconstructPath)
 
-    #result = []
+    result = []
     # 리스트에 window_Size 동안의 데이터를 추가함
     #for dataFileName in dataFileNameList:
     #    data = pd.read_csv(os.path.join(ReconstructPath, dataFileName))
     data = pd.read_csv(os.path.join(ReconstructPath, '005930.csv'))
 
+    """
     scaler = MinMaxScaler()
     scale_cols = ['현재가', '외인순매수거래량', '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']
 
@@ -75,20 +76,22 @@ def LoadData(window_Size):
 
     df_scaled = pd.DataFrame(df_scaled)
     df_scaled.columns = scale_cols
+    """
+    
+    data = data.loc[:, ['현재가', '외인순매수거래량', 
+        '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']]
+    windowSize = window_Size + 1
+    
+    for index in range(len(data) - windowSize):
+    #for index in range(len(data) - windowSize - 12, len(data) - windowSize):
+        stockData = data[index : index + windowSize].copy()
+        result.append(stockData)
+
+    stockData = data[-50:].copy()
+    stockData = Normalize([stockData])
+    result = Normalize(result)
 
     """
-    windowSize = window_Size + 1
-    for index in range(len(data) - windowSize):
-        stockData = data[index : index + windowSize].copy()
-        #stockData = stockData.loc[:, ['종목코드', '현재가', '외인순매수거래량', 
-        #'외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']]
-        stockData = stockData.loc[:, ['현재가', '외인순매수거래량', 
-        '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']]
-        result.append(stockData)
-    
-    result = Normalize(result)
-    """
-    
     row = int(round(df_scaled.shape[0] * 0.9))
     train = df_scaled[:-row]
     test = df_scaled[-row:]
@@ -120,7 +123,10 @@ def LoadData(window_Size):
     x_train = train[:, :-1]
     y_train = train[:, -1, 0]
     x_test = result[row:, :-1]
+    x_test = np.append(x_test, stockData, axis = 0)
     y_test = result[row:, -1, 0]
+    #x_test = result[:, :-1]
+    #y_test = result[:, -1, 0]
 
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2]))
@@ -129,10 +135,22 @@ def LoadData(window_Size):
     print('LoadData time : ' + str(timedelta(seconds = time.time() - start)))
 
     return [x_train, y_train, x_test, y_test]
-    """
 
 # 학습 모델 생성
 def BuildModel():
+    model = Sequential()
+
+    model.add(LSTM(50, input_shape=(50,5), return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(100, return_sequences=False))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(output_dim=1))
+    model.add(Activation('linear'))
+
+    model.compile(loss='mse', optimizer='rmsprop')
+
+    """
     model = Sequential()
     
     model.add(LSTM(16, 
@@ -142,7 +160,7 @@ def BuildModel():
           )
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
-
+    """
     """
     model.add(LSTM(50, return_sequences = True, input_shape = (50, 6)))
     model.add(LSTM(64, return_sequences = False))
@@ -152,6 +170,7 @@ def BuildModel():
 
     return model
 
+"""
 def make_dataset(data, label, window_size=50):
     feature_list = []
     label_list = []
@@ -159,13 +178,14 @@ def make_dataset(data, label, window_size=50):
         feature_list.append(np.array(data.iloc[i:i+window_size]))
         label_list.append(np.array(label.iloc[i+window_size]))
     return np.array(feature_list), np.array(label_list)
-
+"""
 def Run():
 
-    x_train, x_valid, y_train, y_valid, test_feature, test_label = LoadData(50)
+    #x_train, x_valid, y_train, y_valid, test_feature, test_label = LoadData(50)
 
-    #x_train, y_train, x_test, y_test = LoadData(50)
-    model = BuildModel()
+    x_train, y_train, x_test, y_test = LoadData(50)
+    #model = BuildModel()
+    """
     from keras.callbacks import EarlyStopping, ModelCheckpoint
     early_stop = EarlyStopping(monitor='val_loss', patience=5)
     filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp_checkpoint.h5')
@@ -180,10 +200,25 @@ def Run():
 
     model.load_weights(filename)
     pred = model.predict(test_feature)
+    """
+    
+    #model.fit(x_train, y_train, batch_size=512, nb_epoch=100, validation_split=0.05)
+    #model.save('batch512_epoch100.h5')
 
-    plt.figure(figsize=(12, 9))
-    plt.plot(test_label, label='actual')
-    plt.plot(pred, label='prediction')
+    from keras.models import load_model
+
+    model = load_model('batch512_epoch100.h5')
+
+    dateLength = 11
+    x_test2 = x_test[-(dateLength + 1):]
+    y_test2 = (y_test[-dateLength:] + 1) * pivotDatas[-dateLength:]
+    pred = model.predict(x_test2)
+    result_predict = []
+    for i in range(-len(pred), 0):
+        result_predict.append((pred[i] + 1) * pivotDatas[i])
+    plt.figure(facecolor = 'white')
+    plt.plot(y_test2, label='actual')
+    plt.plot(result_predict, label='prediction')
     plt.legend()
     plt.show()
 
