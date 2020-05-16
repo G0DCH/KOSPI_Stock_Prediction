1f88d600e2b164f0d3d824b9a8adc871f5028b66
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-05-16 14:05:02 +0900

    Update Run.py

--- Crawler Py Code/LSTM/Run.py
+++ Crawler Py Code/LSTM/Run.py
@@ -68,16 +68,6 @@ def LoadData(window_Size):
     #    data = pd.read_csv(os.path.join(ReconstructPath, dataFileName))
     data = pd.read_csv(os.path.join(ReconstructPath, '005930.csv'))
 
-    """
-    scaler = MinMaxScaler()
-    scale_cols = ['현재가', '외인순매수거래량', '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']
-
-    df_scaled = scaler.fit_transform(data[scale_cols])
-
-    df_scaled = pd.DataFrame(df_scaled)
-    df_scaled.columns = scale_cols
-    """
-    
     data = data.loc[:, ['현재가', '외인순매수거래량', 
         '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']]
     windowSize = window_Size + 1
@@ -91,30 +81,6 @@ def LoadData(window_Size):
     stockData = Normalize([stockData])
     result = Normalize(result)
 
-    """
-    row = int(round(df_scaled.shape[0] * 0.9))
-    train = df_scaled[:-row]
-    test = df_scaled[-row:]
-
-    feature_cols = ['현재가', '외인순매수거래량', '외인순매수거래대금', '연기금순매수거래량', '연기금순매수거래대금']
-    label_cols = ['현재가']
-
-    train_feature = train[feature_cols]
-    train_label = train[label_cols]
-
-    test_feature = test[feature_cols]
-    test_label = test[label_cols]
-
-    # train dataset
-    train_feature, train_label = make_dataset(train_feature, train_label, window_Size)
-
-    from sklearn.model_selection import train_test_split
-    x_train, x_valid, y_train, y_valid = train_test_split(train_feature, train_label, test_size=0.2)
-
-    test_feature, test_label = make_dataset(test_feature, test_label, window_Size)
-
-    return [x_train, x_valid, y_train, y_valid, test_feature, test_label]
-    """
     # 90퍼센트는 train용 10퍼센트는 validate용으로 씀
     row = int(round(result.shape[0] * 0.9))
     train = result[:row, :]
@@ -125,8 +91,6 @@ def LoadData(window_Size):
     x_test = result[row:, :-1]
     x_test = np.append(x_test, stockData, axis = 0)
     y_test = result[row:, -1, 0]
-    #x_test = result[:, :-1]
-    #y_test = result[:, -1, 0]
 
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2]))
@@ -150,39 +114,9 @@ def BuildModel():
 
     model.compile(loss='mse', optimizer='rmsprop')
 
-    """
-    model = Sequential()
-    
-    model.add(LSTM(16, 
-               input_shape=(50, 5), 
-               activation='relu', 
-               return_sequences=False)
-          )
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    """
-    """
-    model.add(LSTM(50, return_sequences = True, input_shape = (50, 6)))
-    model.add(LSTM(64, return_sequences = False))
-    model.add(Dense(1, activation = 'linear'))
-    model.compile(loss="mse", optimizer="rmsprop")
-    """
-
     return model
 
-"""
-def make_dataset(data, label, window_size=50):
-    feature_list = []
-    label_list = []
-    for i in range(len(data) - window_size):
-        feature_list.append(np.array(data.iloc[i:i+window_size]))
-        label_list.append(np.array(label.iloc[i+window_size]))
-    return np.array(feature_list), np.array(label_list)
-"""
 def Run():
-
-    #x_train, x_valid, y_train, y_valid, test_feature, test_label = LoadData(50)
-
     x_train, y_train, x_test, y_test = LoadData(50)
     #model = BuildModel()
     """
