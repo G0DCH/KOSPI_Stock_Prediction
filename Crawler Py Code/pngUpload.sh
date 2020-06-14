f3b1fff4f90929d7b7e6394eef44117c4353b044
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-06-14 20:10:13 +0900

    Update pngUpload.sh

--- Crawler Py Code/pngUpload.sh
+++ Crawler Py Code/pngUpload.sh
@@ -1,1 +1,3 @@
-aws s3 sync [local path] s3://bucket path --acl public-read
+# [local path]에 PredictPNG의 절대 경로를 대입
+# [bucket path]에 aws s3의 버킷 주소를 대입
+aws s3 sync [local path] s3://[bucket path] --acl public-read
