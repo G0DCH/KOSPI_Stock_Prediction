1a93adaee76797d1ec3c2200b9b331aae1cf1fc3
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-06-23 22:10:29 +0900

    실행 시간 출력

--- Crawler Py Code/UpdateDB.py
+++ Crawler Py Code/UpdateDB.py
@@ -1,10 +1,12 @@
 #-*- coding:utf-8 -*-
 
 import datetime
+import time
 from LSTM.Run_MakeAllPredict import MakeAllPredict
 from LSTM.SaveToDB.SaveToDB import SaveToDB
 import os
 
+start = time.time()
 date = datetime.date.today()
 date = date.strftime('%Y%m%d')
 
@@ -18,5 +20,6 @@ path = os.path.dirname(os.path.abspath(__file__))
 os.system(os.path.join(path, 'pngUpload.sh'))
 
 print(date + ' : Update DB Finished')
+print('Update DB time : ' + str(datetime.timedelta(seconds = time.time() - start)))
 print('*********************************')
 print
